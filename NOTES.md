1cdab0c4fc9fe6df8aa6fd83f50d5b8c7d4703af
Gajaharan Satkunanandan <gajaharan.satkunanandan@whitbread.com>
2020-01-12 23:06:16 +0000

    Testing if Item list is empty

--- NOTES.md
+++ NOTES.md
@@ -5,3 +5,4 @@ Please add here any notes, assumptions and design decisions that might help up u
 - Do not modify existing test where possible.
 - Added AssertJ library to make testing easier for TDD and makes test more readable.
 - Added Mockito library to mock other classes that are not under test.
+- Added apache commons collection utils to test collections validity.
--- src/main/java/kata/supermarket/promotion/BuyThreeForTwoPromotion.java
+++ src/main/java/kata/supermarket/promotion/BuyThreeForTwoPromotion.java
@@ -5,9 +5,15 @@ import kata.supermarket.item.Item;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+
 public class BuyThreeForTwoPromotion implements Promotion{
     @Override
     public BigDecimal applyPromotion(List<Item> items) {
+        if (isEmpty(items)) {
+            return BigDecimal.ZERO;
+        }
+
         return BigDecimal.ZERO;
     }
 }
